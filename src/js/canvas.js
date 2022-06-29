692bbc8f8501d16c8b95c83dff5141d6ab18878c
tylergubser <tylergubser2200@gmail.com>
2022-06-29 12:27:27 -0500

    update

--- src/js/canvas.js
+++ src/js/canvas.js
@@ -254,6 +254,7 @@ const platforms = [
   new Platform({x: 26500, y: 850, image: platformImage}),
   new Platform({x: 27000, y: 850, image: platformImage}),
   new Platform({x: 27500, y: 850, image: platformImage}),
+  new Platform({x: 28000, y: 850, image: platformImage}),
 ]
 
 
